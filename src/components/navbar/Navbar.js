adf39cf5749dd484f2262af85eaae8e78d6f0f11
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-07 21:56:04 +0200

    navbar needs fixes
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,9 +2,21 @@ import React, { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
+  const hideItems = isMenuOpen ? '' : 'hidden'
+
   return (
     <div id="navbar" className="nav">
-      <ul className="nav-list">
+      <div className="menu-icon" onClick={handleToggleMenu}>
+        ☰
+      </div>
+
+      <ul className={`nav-list ${hideItems}`}>
         <li className="logo">
           <a href="#welcome-section">
             Karyna<span>.</span>
@@ -12,6 +24,9 @@ const Navbar = () => {
         </li>
         <li>
           <a href="#welcome-section">About</a>
+        </li>
+        <li>
+          <a href="#skills">Stack</a>
         </li>
         <li>
           <a href="#projects">Work</a>
