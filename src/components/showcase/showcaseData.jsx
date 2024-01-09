dd6ec9d661870e688a95989c133692abd9010063
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-09 17:01:37 +0200

    showcaseData.jsx added, showace map method added
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

---
+++ src/components/showcase/showcaseData.jsx
@@ -0,0 +1,44 @@
+const showcaseData = [
+  {
+    name: 'Project 1',
+    img: './src/assets/img/coder.png',
+    alt: 'descr of proj img',
+    links: {
+      github: 'https://github.com/example1',
+      demo: 'https://example1.com',
+    },
+    description: <p>This is a web site for project 1.</p>,
+  },
+  {
+    name: 'Project 2',
+    img: './src/assets/img/coder.png',
+    alt: 'descr of proj img',
+    links: {
+      github: 'https://github.com/example1',
+      demo: 'https://example1.com',
+    },
+    description: <p>This is a web site for project 2.</p>,
+  },
+  {
+    name: 'Project 3',
+    img: './src/assets/img/coder.png',
+    alt: 'descr of proj img',
+    links: {
+      github: 'https://github.com/example1',
+      demo: 'https://example1.com',
+    },
+    description: <p>This is a web site for project 3.</p>,
+  },
+  {
+    name: 'Project 4',
+    img: './src/assets/img/coder.png',
+    alt: 'descr of proj img',
+    links: {
+      github: 'https://github.com/example1',
+      demo: 'https://example1.com',
+    },
+    description: <p>This is a web site for project 4.</p>,
+  },
+]
+
+export default showcaseData
