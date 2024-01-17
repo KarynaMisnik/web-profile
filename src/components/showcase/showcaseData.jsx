eebc483b932ea77afcc5baa55a05982c4f9567aa
KarynaMisnik <karinamisnik94@gmail.com>
2024-01-17 16:01:16 +0200

    showcase img uploaded, cv data updated
    Signed-off-by: KarynaMisnik <karinamisnik94@gmail.com>

--- src/components/showcase/showcaseData.jsx
+++ src/components/showcase/showcaseData.jsx
@@ -1,42 +1,60 @@
-import MyImage from '../../assets/img/coder.png'
+import TinyTechExhibits from '../../assets/img/Tiny Tech Exhibits.png'
+import HoneyandProduct from '../../assets/img/Honey-SPA-shop.png'
+import FinnishEmblem from '../../assets/img/FinnishEmblem.png'
 
 const showcaseData = [
   {
     name: 'Honey&Products',
-    img: MyImage,
-    alt: 'descr of proj img',
+    img: HoneyandProduct,
+    alt: 'Preview of Honey&Products SPA web store',
     links: {
-      github: 'https://github.com/example1',
+      github: 'https://github.com/KarynaMisnik/bee_e-spa',
       demo: 'https://example1.com',
     },
     description: (
       <p>
-        Web store of a small family business where you can find a good selection
-        of honey with amazing aroma and flavour. If you are a
-        beekeeper-enthusiast or just want to learn more about bees is possible
-        to visit a farm.
+        Web store of an imaginary family business that produces honey, sweets,
+        and cosmetics based on honey. Make yourself familiar with the family
+        story, selection of shop items, and services offered by Honey &
+        Products.
       </p>
     ),
   },
   {
     name: 'TinyTechExhibits',
-    img: MyImage,
-    alt: 'descr of proj img',
+    img: TinyTechExhibits,
+    alt: 'Prewiew of Tiny Tech Exhibits web project',
     links: {
-      github: 'https://github.com/example1',
-      demo: 'https://example1.com',
+      github: 'https://github.com/KarynaMisnik/tiny-tech-exhibits',
+      demo: 'https://karynamisnik.github.io/tiny-tech-exhibits/#',
     },
-    description: <p>This is a web site for project 2.</p>,
+    description: (
+      <p>
+        This project serves as a dynamic playground for honing my HTML, CSS, and
+        JavaScript skills. Since 2022, I've consolidated various small projects
+        to practice understanding programming logic, implementing algorithms,
+        and working with intricate layouts and advanced CSS.
+      </p>
+    ),
   },
   {
     name: 'Finnish Emblem',
-    img: MyImage,
-    alt: 'descr of proj img',
+    img: FinnishEmblem,
+    alt: 'Preview of Finnish Emblem web app about Finnish coat of arms',
     links: {
-      github: 'https://github.com/example1',
+      github: 'https://github.com/KarynaMisnik/coat-of-arms',
       demo: 'https://example1.com',
     },
-    description: <p>Lorem ipsum </p>,
+    description: (
+      <p>
+        This project is currently under development. You can explore it by
+        referring to the GitHub README. The primary focus of the project
+        revolves around the regional structure of Finland. It serves two main
+        purposes: educational and inspirational. My main goal in building this
+        app is to inspire residents, citizens, and tourists to travel around
+        Finland and appreciate its beauty.
+      </p>
+    ),
   },
 ]
 
