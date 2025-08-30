8b2ae6594793e1605f0bd6e986bb1bc5e1d7f7aa
KarynaMisnik <karinamisnik94@gmail.com>
2025-08-30 12:08:00 +0300

    path corrected

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let isOn = false;
 
 switchEl.addEventListener("click", () => {
   isOn = !isOn;
-  switchEl.src = isOn ? "images/switch-on.png" : "images/switch.png";
+  switchEl.src = isOn ? "./images/switch-on.png" : "./images/switch.png";
   portraitEl.src = isOn
-    ? "images/portfolio-pic-colored.png"
-    : "images/portfolio-pic.png";
+    ? "./images/portfolio-pic-colored.png"
+    : "./images/portfolio-pic.png";
 });
